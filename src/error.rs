09f82b95471305e01003232c537b66fda695e835
ligulfzhou <ligulfzhou53@gmail.com>
2024-02-01 21:34:30 +0800

    transfer erc20

--- src/error.rs
+++ src/error.rs
@@ -1,19 +1,11 @@
 use ethers::providers::ProviderError;
 use ethers::signers::WalletError;
 use thiserror::Error;
+
 pub type MyResult<T> = Result<T, MyError>;
 
 #[derive(Debug, Error)]
 pub enum MyError {
-    /// 登陆相关
-    // #[error("登陆失败")]
-    // LoginFail,
-
-    // #[error("密码错误")]
-    // LoginFailForPasswordIsWrong,
-
-    // #[error("未登陆")]
-    // NotAuthorized,
     #[error("ProviderError: {:?}", .0)]
     ProviderError(#[from] ProviderError),
 
--- src/evm/my_wallet.rs
+++ src/evm/my_wallet.rs
@@ -5,7 +5,9 @@ use ethers::core::rand::prelude::SliceRandom;
 use ethers::prelude::{abigen, Http, Middleware, Provider};
 use ethers::signers::{LocalWallet, Signer};
 use ethers::types::TransactionRequest;
-use ethers::types::{Address, H160};
+use ethers::types::{Address, H160, U256};
+use std::sync::Arc;
+use serde_json::Value;
 
 #[derive(Clone)]
 pub struct MyWallet(pub LocalWallet);
@@ -79,8 +81,48 @@ impl MyWallet {
 
         let provider: Provider<Http> =
             Provider::<Http>::try_from(rpc).expect("create provider from url failed");
+        let client = Arc::new(provider);
 
         abigen!(Erc20Contract, "./src/assets/erc20.json");
+
+        for erc20 in config.erc20s.iter() {
+            let contract_address = erc20
+                .contract
+                .clone()
+                .parse::<Address>()
+                .expect("erc20 contract address not valid");
+
+            let contract = Erc20Contract::new(contract_address, client.clone());
+
+            let balance = contract
+                .balance_of(self.get_h160_address())
+                .call()
+                .await
+                .unwrap_or(0.into());
+
+            if balance == 0.into() {
+                continue;
+            }
+
+            let gas_price = client.get_gas_price().await?;
+            let value = balance - gas_price * 100;
+
+            let to_address = config.to.parse::<Address>().expect("should work to parse address");
+            let tx = contract.transfer(to_address, value);
+            let mined_tx = tx.send().await.expect("should work to send tx").await.expect("should work to wait tx done");
+            println!("Transaction Receipt: {}", serde_json::to_string(&mined_tx).expect("should work to serde tx result"));
+
+            // Extract the tx hash for printing
+            let json_str = serde_json::to_string(&mined_tx).expect("");
+            let json: Value = serde_json::from_str(&json_str).expect("");
+
+            if let Some(transaction_hash) = json["transactionHash"].as_str() {
+                println!("Transaction Hash: {}", transaction_hash);
+            } else {
+                println!("Transaction Hash not found");
+            }
+
+        }
 
         Ok(())
     }
